980cfabd5882d9cefd7367d5fa79fd47a413330d
Mackenzie Moore <macmoore@ucdavis.edu>
2019-01-28 21:02:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -10,7 +10,6 @@ def prime_array
   prime_array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 41]
 end
 
-require "pry"
 def prime?(number)
 prime_array
 remainder_nums = divider_function(number)
@@ -20,7 +19,6 @@ elsif (remainder_nums.include?(0)) || (Math.sqrt(number.abs)%1 == 0)
   return false
 else 
   return true
-  binding.pry
 end
 end
 
