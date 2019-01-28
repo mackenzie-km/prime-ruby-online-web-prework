0fb4c5fdd9b3b3c72471721d0dc86022e342605d
Mackenzie Moore <macmoore@ucdavis.edu>
2019-01-28 20:58:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,25 +1,26 @@
-require "pry"
 def divider_function(number)
 nums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 41]
 remainder_nums = []
 nums.each do |num|
     remainder_nums.push(number%num)
 end
-binding.pry
 end
 
 def prime_array
   prime_array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 41]
 end
 
+require "pry"
 def prime?(number)
 prime_array
+divider_function(number)
 if (prime_array.include?(number)) 
   return true
 elsif (remainder_nums.include?(0)) || (Math.sqrt(number.abs)%1 == 0)
   return false
 else 
   return true
+  binding.pry
 end
 end
 
