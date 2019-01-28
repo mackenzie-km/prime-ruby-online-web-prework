0aab83a87660c5288e689750c1f63b91bd740100
Mackenzie Moore <macmoore@ucdavis.edu>
2019-01-28 21:03:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
+require "pry"
 def divider_function(number)
 nums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 41]
 remainder_nums = []
 nums.each do |num|
     return remainder_nums.push(number%num)
+    binding.pry
 end
 end
 
